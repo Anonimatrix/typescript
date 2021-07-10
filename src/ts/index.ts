e4b5a0741707bc1ee3a9b460bdec5a5201fb9a40
Ezequiel Imaz <tavelliezequiel@gmail.com>
2021-07-10 14:32:07 -0300

    Agregue ejemplos de uso de typescript como pueden ser: aserciones de tipo, alias de types, interfaces, extends,enums, tuplas y tipos literales

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,4 @@
+export {};
 let a = "a";
 
 //Number
@@ -26,3 +27,51 @@ if(prompt('Lol')){
 
 octalFunction();
 }
+
+//Array de Arrays de strings
+let mauros: string[][] = [['dsa', 'dsa'], ['dsa', 'dsa']];
+
+//Tuplas
+let lols: [number, number, string, boolean] = [23, 23, 'sda', true];
+
+//Tipos literales
+type literal = 4 | 6 | 8;
+let hilosProcesador: literal = 4;
+
+//Alias
+type ns = number | string;
+let password: ns = 2321321321;
+
+//Enums
+enum Colors {
+    Red = '#f74',
+    Blue = '#62f',
+    Green='#0f7'
+}
+
+let red: Colors = Colors.Red;
+
+//Interfaces
+interface Vehiculo {
+    readonly velocidad: number,
+    readonly modelo: string,
+    readonly marca: string,
+}
+
+//Extends interfaces
+
+interface Coche extends Vehiculo{
+    ruedas?: string,
+}
+
+let Nissan: Coche = {
+    velocidad: 10,
+    modelo: 'Speed',
+    marca: 'Nissan',
+    ruedas: '3x3'
+}
+
+//Aserciones de tipo | Bracket Angle | Desactivado en no-angle-bracket-type-assertion: true
+let lol: any = 'asda';
+(<string>lol).substr(2);
+(lol as string).substr(2);
